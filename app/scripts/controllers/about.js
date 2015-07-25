aaf51d4830765a7f34b29f996fdd4b223f6c42cb
ell <ell.wu@icloud.com>
2015-07-25 09:59:36 +0800

    generate angular structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lwebFaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lwebFaceApp
+ */
+angular.module('lwebFaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lwebFaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lwebFaceApp
+ */
+angular.module('lwebFaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
